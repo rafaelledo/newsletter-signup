82878bc501cb8c586fc73a40cefe77ede61b2ea4
rafaelledo <rafaeloliveiraledo@gmail.com>
2022-08-13 00:06:08 -0300

    second commit

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ const app = express()
 
 app.use(bodyParser.urlencoded({urlencoded: true}))
 
+app.use(express.static("public"))
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", function (req, res) {
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const email = req.body.email
+
+    res.send("Successfully registered on newsletter")
+})
+
 app.listen(80, function () {
     console.log("Server is running on port 80.");
 })
