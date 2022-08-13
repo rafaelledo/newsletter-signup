1a761dabb6e6759380b591b140904c58f8b572fd
rafaelledo <rafaeloliveiraledo@gmail.com>
2022-08-13 20:23:18 -0300

    fifth commit

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.post("/failure", function(req, res) {
     res.redirect("/")
 })
 
-app.listen(80, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running on port 80.");
 })
 
